1048e789a952f62ccfb692a2c8aafe6c18ab685f
전철호 <cheolho@wisoft.io>
2021-05-29 19:35:43 +0900

    Initailizing project

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.0"
+    id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
+}
+
+group = "me.jeoncheolho"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
+}
